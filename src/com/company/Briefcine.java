36c5805ea106b26bec4d6b9ea35afe1d5381081b
nadia j <n.jouini@free.fr>
2021-04-27 19:28:18 +0200

    2704

--- src/com/company/Briefcine.java
+++ src/com/company/Briefcine.java
@@ -6,17 +6,16 @@ import java.util.Scanner;
 public class Briefcine {
 
     //creation fonction pour afficher la salle
+
     private static void affichagedelasalle(String[][] salle) {
         for (int i = 0; i < salle.length; i++) {
             for (int j = 0; j < salle[i].length; j++) {
-
-
                 System.out.print("[" + salle[i][j] + "]");
             }
             System.out.println();
         }
-
     }
+
 
     public static void main(String[] args) {
 
@@ -24,7 +23,7 @@ public class Briefcine {
 
         Scanner sc = new Scanner(System.in);
 
-        //Déclaration du tableau pour representer la salle de cinema
+        //Initialisation du tableau pour representer la salle de cinema
 
         String[][] salle = new String[7][10];
 
@@ -42,66 +41,120 @@ public class Briefcine {
             System.out.println();
         }
 
-
-//Demarrage grande boucle pour répéter l'ensemble du code ci dessous
-
-        for (int a = 0; a < 10; a++) {
+        //creation de la grande boucle pour repeter le code
 
 
-            System.out.println("Bonjour et bienvenue dans votre cinéma!");
+        System.out.println("Bonjour et bienvenue dans votre cinéma!");
+
+
+        Integer[] positionSurLaRangee = new Integer[7];
+        positionSurLaRangee[0] = 0;
+        positionSurLaRangee[1] = 0;
+        positionSurLaRangee[2] = 0;
+        positionSurLaRangee[3] = 0;
+        positionSurLaRangee[4] = 0;
+        positionSurLaRangee[5] = 0;
+        positionSurLaRangee[6] = 0;
+
+
 
 //demander au client quelle rangée reserver
 
-            System.out.println("Pour reserver ,merci de choisir votre rangée de 1 a 7: ");
-            int rangeeDemandee = sc.nextInt();
+        System.out.println("Pour reserver ,merci de choisir votre rangée de 1 a 7: ");
+        int rangeeDemandee = sc.nextInt();
+        sc.nextLine();
+        if (rangeeDemandee > 7) {
+            System.out.println("Merci de ressaisir un nombre de places entre 1 et 7: ");
+            rangeeDemandee = sc.nextInt();
             sc.nextLine();
-            if (rangeeDemandee > 7) {
-                System.out.println("Merci de ressaisir un nombre de places entre 1 et 7: ");
-                rangeeDemandee = sc.nextInt();
-                sc.nextLine();
-            }
+        }
 
 
 //demander au client combien de places il souhaite reserver
 
-            System.out.println("Combien de place souhaitez vous reserver(max 10): ");
-            int placesAreserver = sc.nextInt();
+        System.out.println("Combien de places souhaitez vous reserver(max 10): ");
+        int placesAreserver = sc.nextInt();
+        sc.nextLine();
+
+        if (placesAreserver > 10) {
+            System.out.println("Merci de ressaisir un nombre de places entre 0 et 10: ");
+            placesAreserver = sc.nextInt();
             sc.nextLine();
-
-            if (placesAreserver > 10) {
-                System.out.println("Merci de ressaisir un nombre de places entre 0 et 10: ");
-                placesAreserver = sc.nextInt();
-                sc.nextLine();
-            }
-
-//Boucle pour parcourir la salle de cinema en y incluant les informations recuperees precedemment (sans imprimer le tableau)
+        }
 
 
+        boolean siegeDispo = true;
 
-            for (int l = 0; l < salle.length; l++) {
-                if (l == rangeeDemandee - 1) { //on precise le -1 pour prendre en compte le demarrage à 0 de l'index du tableau
-                    for (int c =0; c < salle[l].length && c < placesAreserver; c++) {
-                        if (salle[l][c] == "-") {
-                            salle[l][c] = "X";
+        while (siegeDispo) {
+
+            for (int i = 0; i < salle.length; i++) {
+
+                if (i == rangeeDemandee - 1) {
+                    int position = positionSurLaRangee[i];
+
+                    for (int j = position; j < position + placesAreserver && j < salle[i].length; j++) {
+
+                        if (salle[i][j] == "-") {
+                            salle[i][j] = "x";
+                            positionSurLaRangee[i]++;
+                            System.out.println(positionSurLaRangee[i]);
+                            if (positionSurLaRangee[i] >= 10) {
+                                System.out.println("Plus de places disponibles sur la rangee " + rangeeDemandee);
+                            }
 
                         }
 
+
+                    }
+                    affichagedelasalle(salle);
+
+
+
+
+                    //demander au client quelle rangée reserver
+
+                    System.out.println("Pour reserver ,merci de choisir votre rangée de 1 a 7: ");
+                    rangeeDemandee = sc.nextInt();
+                    sc.nextLine();
+                    if (rangeeDemandee > 7) {
+                        System.out.println("Merci de ressaisir un nombre de places entre 1 et 7: ");
+                        rangeeDemandee = sc.nextInt();
+                        sc.nextLine();
                     }
 
+
+//demander au client combien de places il souhaite reserver
+
+                    System.out.println("Combien de places souhaitez vous reserver(max 10): ");
+                    placesAreserver = sc.nextInt();
+                    sc.nextLine();
+
+                    if (placesAreserver > 10) {
+                        System.out.println("Merci de ressaisir un nombre de places entre 0 et 10: ");
+                        placesAreserver = sc.nextInt();
+                        sc.nextLine();
+                    }
+
+
                 }
-
-                //on reimprime le tableau avec les informations
-
-                affichagedelasalle(salle);
-
             }
-
         }
-
     }
 
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
