df164e2aa386c85d6c14468cd283acea7e0c7fa9
nadia j <n.jouini@free.fr>
2021-04-28 12:01:02 +0200

    V2 avec corrections

--- src/com/company/Briefcine.java
+++ src/com/company/Briefcine.java
@@ -41,11 +41,13 @@ public class Briefcine {
             System.out.println();
         }
 
-        //creation de la grande boucle pour repeter le code
+
 
 
         System.out.println("Bonjour et bienvenue dans votre cinéma!");
+        System.out.println();
 
+        //declaration de quelques variables
 
         Integer[] positionSurLaRangee = new Integer[7];
         positionSurLaRangee[0] = 0;
@@ -56,18 +58,19 @@ public class Briefcine {
         positionSurLaRangee[5] = 0;
         positionSurLaRangee[6] = 0;
 
-
+        boolean siegeDispo = true;
 
 //demander au client quelle rangée reserver
 
-        System.out.println("Pour reserver ,merci de choisir votre rangée de 1 a 7: ");
+        System.out.println("Pour réserver ,merci de choisir votre rangée de 1 a 7: ");
         int rangeeDemandee = sc.nextInt();
         sc.nextLine();
         if (rangeeDemandee > 7) {
-            System.out.println("Merci de ressaisir un nombre de places entre 1 et 7: ");
+            System.err.print("Rangée inexistante ! Merci de ressaisir un numéro de rangée entre entre 1 et 7: ");
             rangeeDemandee = sc.nextInt();
             sc.nextLine();
         }
+
 
 
 //demander au client combien de places il souhaite reserver
@@ -83,7 +86,7 @@ public class Briefcine {
         }
 
 
-        boolean siegeDispo = true;
+
 
         while (siegeDispo) {
 
@@ -97,9 +100,14 @@ public class Briefcine {
                         if (salle[i][j] == "-") {
                             salle[i][j] = "x";
                             positionSurLaRangee[i]++;
+
                             System.out.println(positionSurLaRangee[i]);
-                            if (positionSurLaRangee[i] >= 10) {
-                                System.out.println("Plus de places disponibles sur la rangee " + rangeeDemandee);
+
+                            if (positionSurLaRangee[i] >= 10 || placesAreserver >salle[i].length) {
+
+                                System.err.print("Plus de places disponibles sur la rangee " + rangeeDemandee);
+                                System.out.println();
+
                             }
 
                         }
@@ -109,15 +117,13 @@ public class Briefcine {
                     affichagedelasalle(salle);
 
 
-
-
                     //demander au client quelle rangée reserver
 
                     System.out.println("Pour reserver ,merci de choisir votre rangée de 1 a 7: ");
                     rangeeDemandee = sc.nextInt();
                     sc.nextLine();
-                    if (rangeeDemandee > 7) {
-                        System.out.println("Merci de ressaisir un nombre de places entre 1 et 7: ");
+                    if (rangeeDemandee > 7 ) {
+                        System.err.print("Rangee inexistante ! Merci de ressaisir une rangee  entre 1 et 7: ");
                         rangeeDemandee = sc.nextInt();
                         sc.nextLine();
                     }
@@ -125,15 +131,9 @@ public class Briefcine {
 
 //demander au client combien de places il souhaite reserver
 
-                    System.out.println("Combien de places souhaitez vous reserver(max 10): ");
+                    System.out.println("Combien de places souhaitez vous reserver: ");
                     placesAreserver = sc.nextInt();
                     sc.nextLine();
-
-                    if (placesAreserver > 10) {
-                        System.out.println("Merci de ressaisir un nombre de places entre 0 et 10: ");
-                        placesAreserver = sc.nextInt();
-                        sc.nextLine();
-                    }
 
 
                 }
